3f26d25b3a9808c7e897b114aebb8485002f4c87
Mukondleteri <Mukondleteri@gmail.com>
2022-11-12 10:19:44 +0200

    Project skeleton

---
+++ src/main/java/com/rank/startup/CasinoStartUpApplication.java
@@ -0,0 +1,11 @@
+package com.rank.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoStartUpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoStartUpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rank/startup/CasinoStartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rank.startup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoStartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
