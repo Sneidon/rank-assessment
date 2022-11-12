c2389b17ad2482caf276667217bf96c74bcb873c
Mukondleteri <Mukondleteri@gmail.com>
2022-11-12 11:04:17 +0200

    Removed unused imports

--- src/main/java/com/rank/startup/controllers/HealthCheckController.java
+++ src/main/java/com/rank/startup/controllers/HealthCheckController.java
@@ -2,16 +2,13 @@ package com.rank.startup.controllers;
 
 import com.rank.startup.dto.HealthDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Slf4j
 @RestController
